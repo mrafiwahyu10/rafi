0ace20af4f6d3288be1065738c941e6076ade842
mrafiwahyu10 <125549959+mrafiwahyu10@users.noreply.github.com>
2023-03-24 15:39:54 +0700

    latihan array 1
    latihan array minggu 7

--- latihan1/minggu7/array1.php
+++ latihan1/minggu7/array1.php
@@ -1,7 +1,7 @@
 <?php
 // perulangan pada array
 // for atau foreach
-$angka = [1, 3, 5, 7 ,9];
+$angka = [1, 3, 5, 7 ,9, 11, 13, 15, 17];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,15 +11,33 @@ $angka = [1, 3, 5, 7 ,9];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan Array</title>
     <style>
+        .kotak{
+            width: 50px;
+            height: 50px;
+            background-color: yellow;
+            text-align: center;
+            line-height: 50px;
+            margin: 3px;
+            float: left;
+        }
+        .clear{
+            clear: both
+        }
     </style>
 </head>
 
 <body>
-    <?php for($i = 0 ; $i < 5; $i++) { ?>
-    <div> class="kotak">
-        <?php echo $angka[$i] ?>
+    <?php for ($i = 0; $i <= 7; $i++) { ?>
+    <div class="kotak">
+        <?= $angka[$i]; ?>
     </div>
     <?php } ?>
+    <div class="clear"></div>
+    <?php foreach($angka as $a) : ?>
+        <div class="kotak">
+            <?= $a; ?>
+        </div>
+    <?php endforeach; ?>
 </body>
 
 </html>
